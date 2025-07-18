92ef5ed7c5d1509d4dd877535f7d4f5e01612399
jara96 <fvjara96@gmail.com>
2025-07-17 23:14:21 -0300

    creacion de springboot_biblioteca

---
+++ springboot_biblioteca/src/test/java/com/confi/spring/biblioteca/springboot_biblioteca/SpringbootBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confi.spring.biblioteca.springboot_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
